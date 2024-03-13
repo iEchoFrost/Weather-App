97202e9e3101aba5e519565dc169529949bc000a
iEchoFrost <101670524+iEchoFrost@users.noreply.github.com>
2024-03-12 21:23:51 -0400

    Update index.js
    Cleanup of unused functions.
    Applied [querySelector] for icon data with class and width.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,3 @@
-//Weather prompt
-//--window.onload =
-function cityWeather() {
-  let weather = {
-    paris: {
-      temp: 19.7,
-      humidity: 80,
-    },
-    tokyo: {
-      temp: 17.3,
-      humidity: 50,
-    },
-    lisbon: {
-      temp: 30.2,
-      humidity: 20,
-    },
-    "san francisco": {
-      temp: 20.9,
-      humidity: 100,
-    },
-    oslo: {
-      temp: -5,
-      humidity: 20,
-    },
-  };
-
-  let city = prompt("Enter a city.").toLowerCase();
-
-  if (weather[city] !== undefined) {
-    let temperature = weather[city].temp;
-    let humidity = weather[city].humidity;
-    alert(
-      `It is currently ${Math.round(temperature)}°C (${Math.round(
-        (temperature * 9) / 5 + 32
-      )}°F) in ${city} with a humidity of ${humidity}%`
-    );
-  } else {
-    alert(
-      `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+in+${city}`
-    );
-  }
-}
-
 //Present Time.
 window.onload = function currentTime() {
   let date = new Date();
@@ -79,6 +36,10 @@ function search(event) {
   function searchWeather(response) {
     console.log(response.data);
 
+    document.querySelector(
+      ".currentCityIcon"
+    ).innerHTML = `<img class="activeIcon" src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png" width="140px" />`;
+
     document.querySelector(`.activeTemp`).innerHTML = `${Math.round(
       response.data.temperature.current
     )}°F`;
@@ -106,6 +67,10 @@ document.querySelector("#search-form").addEventListener("submit", search);
 
 function currentCityWeather(response) {
   console.log(response.data);
+
+  document.querySelector(
+    ".currentCityIcon"
+  ).innerHTML = `<img class="activeIcon" src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png" width="140px" />`;
 
   document.querySelector("#this-city").innerHTML =
     response.data.city.toUpperCase();
